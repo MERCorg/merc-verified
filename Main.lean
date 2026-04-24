b848553b8de0c98222071c3b400a89b3ba388c14
Maurice Laveaux <m.laveaux@tue.nl>
2026-04-24 15:31:56 +0200

    Added basic definitions for signatures and branching bisimulation

---
+++ Main.lean
@@ -0,0 +1,11 @@
+import MercVerified
+
+/-- The implementation of the LTS -/
+structure LabelledTransitionSystem (State Label : Type) where
+  Outgoing : (s : State) → (Label × State) → Prop
+
+/-- An implementation of the signature computation for a labelled transition system. -/
+def compute_signature (lts: LabelledTransitionSystem State Label) (s: State) : Set (Label × State) :=
+  { (μ, s') | lts.Outgoing s (μ, s') }
+
+def x := LabelledTransitionSystem.mk (λ s: State, ("a", s));
---
+++ MercVerified/Basic.lean
@@ -0,0 +1,2 @@
+import MercVerified.BranchingBisimilarity
+import MercVerified.Signature
