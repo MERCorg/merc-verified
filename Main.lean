b785accb5f8c9c74855373f092323a13403bcb60
Maurice Laveaux <m.laveaux@tue.nl>
2026-04-24 15:59:41 +0200

    Removed the main since we are only interesting in proving.

--- Main.lean
+++
@@ -1,9 +0,0 @@
-import MercVerified
-
-/-- The implementation of the LTS -/
-structure LabelledTransitionSystem (State Label : Type) where
-  Outgoing : (s : State) → (Label × State) → Prop
-
-/-- An implementation of the signature computation for a labelled transition system. -/
-def compute_signature (lts: LabelledTransitionSystem State Label) (s: State) : Set (Label × State) :=
-  { (μ, s') | lts.Outgoing s (μ, s') }
