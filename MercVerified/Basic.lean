97d08ba89fa04f90497980451eb03e9593faf0a1
Maurice Laveaux <m.laveaux@tue.nl>
2026-04-25 16:21:35 +0200

    Further improvements, import generated code and ignore artifacts.

--- MercVerified/Basic.lean
+++ MercVerified/Basic.lean
@@ -1,2 +1,7 @@
 import MercVerified.BranchingBisimilarity
 import MercVerified.Signature
+
+-- Import the generated Lean code
+import MercVerified.code.Funs;
+import MercVerified.code.FunsExternal_Template;
+import MercVerified.code.Types;
