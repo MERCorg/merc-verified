817b07616116579a0b49762fb39d968f7e71c8e0
Maurice Laveaux <m.laveaux@tue.nl>
2026-05-13 13:52:21 +0200

    Fixed compilation issues

--- MercVerified/Basic.lean
+++ MercVerified/Basic.lean
@@ -2,7 +2,7 @@ import Signatures.BranchingBisimilarity
 import Signatures.Signature
 
 -- Import the generated Lean code
-import MercVerified.Code.Funs;
-import MercVerified.Code.FunsExternal_Template;
-import MercVerified.Code.FunsExternal;
-import MercVerified.Code.Types;
+import MercVerified.Code.Funs
+import MercVerified.Code.FunsExternal_Template
+import MercVerified.Code.FunsExternal
+import MercVerified.Code.Types
