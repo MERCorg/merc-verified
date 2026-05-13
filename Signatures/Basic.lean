2987d64c9e94a0695120f7da2b467deb549871a4
Maurice Laveaux <m.laveaux@tue.nl>
2026-05-13 13:32:01 +0200

    Depend on merc, and modified the libraries

--- Signatures/Basic.lean
+++ Signatures/Basic.lean
@@ -1,7 +1,6 @@
 import Signatures.BranchingBisimilarity
 import Signatures.Signature
 
-
 -- The implementation of an LTS -
 structure LabelledTransitionSystem (State Label : Type) where
   Outgoing : (s : State) → (Label × State) → Prop
@@ -9,6 +8,3 @@ structure LabelledTransitionSystem (State Label : Type) where
 -- An implementation of the signature computation for a labelled transition system.
 def compute_signature (lts: LabelledTransitionSystem State Label) (s: State) : Set (Label × State) :=
   { (μ, s') | lts.Outgoing s (μ, s') }
-
-def refines (lts: LabelledTransitionSystem State Label) (s: State) (partition: State → Block) : Set (Label × Block) :=
-  { (μ, α) | ∃ s', lts.Outgoing s (μ, s') ∧ partition s' = α }
