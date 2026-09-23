42dfe0847dedeb123e09fd0915227ad8d70e7980
Maurice Laveaux <m.laveaux@tue.nl>
2026-09-23 11:41:38 +0200

    Removed the TagIndex Eq workaround, its fixed upstream.

--- MercVerified/Code/FunsExternal.lean
+++ MercVerified/Code/FunsExternal.lean
@@ -2,15 +2,6 @@ import MercVerified.Code.FunsExternal_Template
 
 open Aeneas Aeneas.Std Result
 
-/--
-I don't quite understand why this is necessary, but it seems that Aeneas derives
-a (infinite) recursive definitions for the impl<T: Eq> Eq for TaggedIndex<T> {} case.
--/
-@[trait_default]
-def core.cmp.Eq.assert_fields_are_eq.default
-  {Self : Type} (_ : core.cmp.Eq Self) (_ : Self) : Result Unit :=
-  .ok ()
-
 def
   merc_utilities.tagged_index.TagIndex.Insts.CoreCmpEq.assert_fields_are_eq
   {T : Type} (Tag : Type) (_corecmpEqInst : core.cmp.Eq T)
